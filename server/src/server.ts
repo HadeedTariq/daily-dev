5a1afa2fea45a194923ef2ae29a953e3cd09b3de
Muhammad Hadeed Tariq <120406067+HadeedTariq@users.noreply.github.com>
2024-11-17 22:44:14 +0500

    make some changes in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,8 @@ import helmet from "helmet";
 import { pino } from "pino";
 
 import { openAPIRouter } from "@/api-docs/openAPIRouter";
-import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
-import { userRouter } from "@/api/user/userRouter";
 import errorHandler from "@/common/middleware/errorHandler";
-import rateLimiter from "@/common/middleware/rateLimiter";
-import requestLogger from "@/common/middleware/requestLogger";
-import { env } from "@/common/utils/envConfig";
+import requestLogger from "./common/middleware/requestLogger";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -20,16 +16,13 @@ app.set("trust proxy", true);
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(helmet());
-app.use(rateLimiter);
 
 // Request logging
 app.use(requestLogger);
 
 // Routes
-app.use("/health-check", healthCheckRouter);
-app.use("/users", userRouter);
 
 // Swagger UI
 app.use(openAPIRouter);
